93c51b75193126fbb0fbefec2873cbd199c8be95
CorzoMatias <matimcorzo@gmail.com>
2025-07-01 13:40:22 -0300

    Se agrega logica para el controlador de detalle-noticia como asi tambien contenido a su vista.

--- src/app/caracteristicas/noticias/paginas/detalle-noticia/detalle-noticia.component.ts
+++ src/app/caracteristicas/noticias/paginas/detalle-noticia/detalle-noticia.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Noticia } from '../../../../core/modelos/noticia';
+import { ServicioNoticiasService } from '../../../../core/servicios/servicio-noticias.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detalle-noticia',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './detalle-noticia.component.html',
-  styleUrl: './detalle-noticia.component.scss'
+  styleUrls: ['./detalle-noticia.component.scss']
 })
-export class DetalleNoticiaComponent {
+export class DetalleNoticiaComponent implements OnInit {
+  noticia!: Noticia | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private servicioNoticias: ServicioNoticiasService
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.noticia = this.servicioNoticias.obtenerNoticiaPorId(id);
+  }
 }
--- src/app/core/servicios/servicio-noticias.service.ts
+++ src/app/core/servicios/servicio-noticias.service.ts
@@ -9,12 +9,16 @@ import { Noticia } from '../modelos/noticia';
 })
 export class ServicioNoticiasService {
   private noticiasMock: Noticia[] = [
-    { id: 1, titulo: 'Nueva ley de tecnología', contenido: 'Lorem ipsum...', autor: 'María', fecha: new Date('2024-10-01') },
-    { id: 2, titulo: 'Angular 17 lanzado oficialmente', contenido: 'Lorem ipsum...', autor: 'Carlos', fecha: new Date('2025-01-15') },
-    { id: 3, titulo: 'ChatGPT revoluciona la programación', contenido: 'Lorem ipsum...', autor: 'Lucía', fecha: new Date('2025-06-20') }
+    { id: 1, titulo: 'Nueva ley de tecnología', contenido: 'TEST NUMERO 1', autor: 'María', fecha: new Date('2024-10-01') },
+    { id: 2, titulo: 'Angular 17 lanzado oficialmente', contenido: 'TEST NUMERO 2', autor: 'Carlos', fecha: new Date('2025-01-15') },
+    { id: 3, titulo: 'ChatGPT revoluciona la programación', contenido: 'TEST NUMERO 3', autor: 'Lucía', fecha: new Date('2025-06-20') }
   ];
 
   obtenerNoticias(): Noticia[] {
     return this.noticiasMock;
   }
+
+  obtenerNoticiaPorId(id: number): Noticia | undefined {
+  return this.noticiasMock.find(noticia => noticia.id === id);
+}
 }
